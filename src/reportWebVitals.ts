d07132bd38b8526be5561fb01908b16e120ab768
Hitesh Kumar <proudhitesh@gmail.com>
2024-01-01 17:55:31 +0530

    chore: :sparkles: webVital file added with interface

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,8 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler, Metric } from 'web-vitals';
+
+interface WebVitals {
+  getCLS: (onPerfEntry: ReportHandler) => void;
+  getFID: (onPerfEntry: ReportHandler) => void;
+  getFCP: (onPerfEntry: ReportHandler) => void;
+  getLCP: (onPerfEntry: ReportHandler) => void;
+  getTTFB: (onPerfEntry: ReportHandler) => void;
+}
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }: WebVitals) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
